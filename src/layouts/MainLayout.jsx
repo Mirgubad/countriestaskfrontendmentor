ff6078cc535be302f41575e7cb6e320f14c693e8
Mirgubad <mirgubadma@code.edu.az>
2025-03-02 00:31:23 +0400

    footer added

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -8,6 +8,24 @@ const MainLayout = () => {
       <main>
         <Navbar />
         <Outlet />
+        <footer>
+          <div className="text-center my-4">
+            <p>
+              Challenge by{" "}
+              <a
+                href="https://www.frontendmentor.io?ref=challenge"
+                target="_blank"
+              >
+                Frontend Mentor
+              </a>
+              . Coded by{" "}
+              <a href="https://www.linkedin.com/in/mirgubad-akbarov-191b32242/">
+                Mirgubad Akbarov
+              </a>
+              .
+            </p>
+          </div>
+        </footer>
       </main>
     </>
   )
